4d3ccbd4ecddc7fb5dbf87b1ddfd7547176451d3
Tiago Bernardo <hsbtiago@gmail.com>
2018-08-07 03:26:39 -0400

    Migrations executado

---
+++ BackEnd/Migrations/20180807072338_Genesis.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class Genesis : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Imoveis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataRegistro = table.Column<DateTime>(nullable: false),
+                    Endereco_CEP = table.Column<string>(maxLength: 10, nullable: true),
+                    Endereco_Rua = table.Column<string>(maxLength: 50, nullable: true),
+                    Endereco_Numero = table.Column<string>(maxLength: 10, nullable: true),
+                    Endereco_Bairro = table.Column<string>(maxLength: 50, nullable: true),
+                    Endereco_Complemento = table.Column<string>(maxLength: 100, nullable: true),
+                    Endereco_Cidade = table.Column<string>(maxLength: 50, nullable: true),
+                    Endereco_Estado = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imoveis", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locatarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    CPF = table.Column<string>(maxLength: 11, nullable: false),
+                    RG = table.Column<string>(maxLength: 20, nullable: true),
+                    DataRegistro = table.Column<DateTime>(nullable: false),
+                    Endereco_CEP = table.Column<string>(maxLength: 10, nullable: true),
+                    Endereco_Rua = table.Column<string>(maxLength: 50, nullable: true),
+                    Endereco_Numero = table.Column<string>(maxLength: 10, nullable: true),
+                    Endereco_Bairro = table.Column<string>(maxLength: 50, nullable: true),
+                    Endereco_Complemento = table.Column<string>(maxLength: 100, nullable: true),
+                    Endereco_Cidade = table.Column<string>(maxLength: 50, nullable: true),
+                    Endereco_Estado = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locatarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Garantia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FiadorId = table.Column<int>(nullable: true),
+                    valorCaucao = table.Column<decimal>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Garantia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Garantia_Locatarios_FiadorId",
+                        column: x => x.FiadorId,
+                        principalTable: "Locatarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contratos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataRegistro = table.Column<DateTime>(nullable: false),
+                    Inicio = table.Column<DateTime>(nullable: false),
+                    Termino = table.Column<DateTime>(nullable: false),
+                    Observacao = table.Column<string>(maxLength: 300, nullable: true),
+                    LocatarioId = table.Column<int>(nullable: true),
+                    ImovelId = table.Column<int>(nullable: true),
+                    GarantiaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contratos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contratos_Garantia_GarantiaId",
+                        column: x => x.GarantiaId,
+                        principalTable: "Garantia",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Contratos_Imoveis_ImovelId",
+                        column: x => x.ImovelId,
+                        principalTable: "Imoveis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Contratos_Locatarios_LocatarioId",
+                        column: x => x.LocatarioId,
+                        principalTable: "Locatarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Aluguel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Vencimento = table.Column<DateTime>(nullable: false),
+                    Valor = table.Column<decimal>(nullable: false),
+                    ValorPago = table.Column<decimal>(nullable: true),
+                    DataPagamento = table.Column<DateTime>(nullable: true),
+                    observacao = table.Column<string>(maxLength: 500, nullable: true),
+                    ContratoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aluguel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Aluguel_Contratos_ContratoId",
+                        column: x => x.ContratoId,
+                        principalTable: "Contratos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aluguel_ContratoId",
+                table: "Aluguel",
+                column: "ContratoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contratos_GarantiaId",
+                table: "Contratos",
+                column: "GarantiaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contratos_ImovelId",
+                table: "Contratos",
+                column: "ImovelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contratos_LocatarioId",
+                table: "Contratos",
+                column: "LocatarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Garantia_FiadorId",
+                table: "Garantia",
+                column: "FiadorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aluguel");
+
+            migrationBuilder.DropTable(
+                name: "Contratos");
+
+            migrationBuilder.DropTable(
+                name: "Garantia");
+
+            migrationBuilder.DropTable(
+                name: "Imoveis");
+
+            migrationBuilder.DropTable(
+                name: "Locatarios");
+        }
+    }
+}
