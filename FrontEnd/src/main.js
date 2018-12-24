ee6a34f91b7aaed00731f7a0e5d054d526c65e89
Tiago Bernardo <hsbtiago@gmail.com>
2018-12-24 16:36:07 -0400

    Remoção de vuetify e vee validate

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,17 +1,12 @@
 import '@babel/polyfill';
-import './plugins/vuetify';
-import './plugins/veevalidate';
 import './plugins/vuepagetitle';
-
 import Vue from 'vue';
 import App from './App.vue';
-
 import router from "./router";
 import VueResource from "vue-resource";
 
 Vue.config.productionTip = false;
 Vue.use(VueResource);
-// Vue.http.options.root = 'https://localhost:5001/api/';
 Vue.http.options.root = 'http://localhost:49459/api/';
 
 new Vue({
--- FrontEnd/src/plugins/veevalidate.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import pt_BR from 'vee-validate/dist/locale/pt_BR';
-import VeeValidate, { Validator } from 'vee-validate';
-
-Validator.localize('pt-BR', pt_BR);
-
-Vue.use(VeeValidate);
